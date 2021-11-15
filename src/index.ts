cc107eeab501a725e55db97ab253377897e53704
Feodor Tobler <feodor-tobler>
2021-11-15 16:14:12 +0100

    fix indenting and adjust come eslint rules

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,12 @@ app.get('/', (_, res) => {
   const a: number = 1;
   const b: number = 2;
   const c = sum(a, b);
-  res.status(200).send(`Hello World old ${c}`);
+  res.status(200).send(`Hello World new ${c}`);
+});
+
+app.get('/evalutation', (req, res) => {
+  res.status(200).send('Hello evaluation');
+  console.log(req.query);
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
