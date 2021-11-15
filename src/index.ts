b6e51edec6941c681797067351459641ab117139
Feodor Tobler <feodor-tobler>
2021-11-15 19:09:13 +0100

    listen to http port 80 and https port 443

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import evaluationHandler from './routes/evaluationHandler';
 
 const app = express();
-const port = 3000;
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
 
 app.use('/evaluation', evaluationHandler);
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+const options = {
+    key: fs.readFileSync(`${__dirname}/certs/key.pem`),
+    cert: fs.readFileSync(`${__dirname}/certs/cert.pem`),
+};
+
+http.createServer(app).listen(HTTP_PORT, () => console.log(`Listening for http request on port ${HTTP_PORT}`));
+https.createServer(options, app).listen(HTTPS_PORT, () => console.log(`Listening for secure https request on port ${HTTPS_PORT}`));
