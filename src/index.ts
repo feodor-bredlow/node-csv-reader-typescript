7be88c6bff17d0dec8e06d0b2cee1fe1c307f88d
Feodor Tobler <feodor-tobler>
2021-11-15 08:38:17 +0100

    add eslint and configure jest

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import express from 'express'
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-  res.status(200).send("Hello World123")
-})
-app.listen(port, () => console.log(`Running on port ${port}`))
+import express from 'express';
+import sum from './sum';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_, res) => {
+  const a: number = 1;
+  const b: number = 2;
+  const c = sum(a, b);
+  res.status(200).send(`Hello World old ${c}`);
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
