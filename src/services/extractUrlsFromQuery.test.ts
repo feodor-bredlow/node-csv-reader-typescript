622c6783307afe1214635bf326d74f96bace5ad0
Feodor Tobler <feodor-tobler>
2021-11-15 18:26:03 +0100

    add extractUrlsFromQuery test for empty query body

--- src/services/extractUrlsFromQuery.test.ts
+++ src/services/extractUrlsFromQuery.test.ts
@@ -1,12 +1,17 @@
 import { ParsedQs } from 'qs';
 import extractUrlsFromQuery from './extractUrlsFromQuery';
 
-test('extractUrlsFromQuery to correctly return urls if url parameter is present', () => {
+test('extractUrlsFromQuery to correctly return urls if "url" parameter is present', () => {
     const queryObject: ParsedQs = { url: ['url0', 'url1', 'url2'], blob: 'foo' };
     expect(extractUrlsFromQuery(queryObject)).toStrictEqual(['url0', 'url1', 'url2']);
 });
 
-test('extractUrlsFromQuery to correctly return empty array if no url parameter is present', () => {
+test('extractUrlsFromQuery to correctly return empty array if no "url" parameter is present', () => {
     const queryObject: ParsedQs = { blubb: ['url0', 'url1', 'url2'], blob: 'foo' };
+    expect(extractUrlsFromQuery(queryObject)).toStrictEqual([]);
+});
+
+test('extractUrlsFromQuery to correctly return empty array empty query body is present', () => {
+    const queryObject: ParsedQs = {};
     expect(extractUrlsFromQuery(queryObject)).toStrictEqual([]);
 });
