1d27373e48b4330fa64ed0cdd48380c8c407aabe
Feodor Tobler <feodor-tobler>
2021-11-15 18:23:11 +0100

    refactor index

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
 import express from 'express';
-import sum from './sum';
+import evaluationHandler from './routes/evaluationHandler';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (_, res) => {
-  const a: number = 1;
-  const b: number = 2;
-  const c = sum(a, b);
-  res.status(200).send(`Hello World new ${c}`);
-});
-
-app.get('/evalutation', (req, res) => {
-  res.status(200).send('Hello evaluation');
-  console.log(req.query);
-});
+app.use('/evaluation', evaluationHandler);
 
 app.listen(port, () => console.log(`Running on port ${port}`));
